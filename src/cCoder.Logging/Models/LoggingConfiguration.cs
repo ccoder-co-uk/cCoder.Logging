dddeff3414e03e56e1497d1195d01c31f69a9304
TehWardy <paul.ward@ccoder.co.uk>
2026-04-15 18:39:31 +0100

    add configurable domain api setup

---
+++ src/cCoder.Logging/Models/LoggingConfiguration.cs
@@ -0,0 +1,21 @@
+using EventLibrary.Models;
+
+namespace cCoder.Logging.Models;
+
+public class LoggingConfiguration
+{
+    public IDictionary<string, string> ConnectionStrings { get; set; } = new Dictionary<string, string>();
+    public IDictionary<string, string> Settings { get; set; } = new Dictionary<string, string>();
+    public IDictionary<string, string> Services { get; set; } = new Dictionary<string, string>();
+    public bool DebugInfo { get; set; }
+    public bool LogSQL { get; set; }
+    public string RootPath { get; set; } = "Api/Logging";
+    public bool IncludeLegacyCoreContext { get; set; } = true;
+    public EventProvider[] EventProviders { get; private set; } = [];
+
+    public LoggingConfiguration WithEventProviders(params EventProvider[] eventProviders)
+    {
+        EventProviders = eventProviders ?? [];
+        return this;
+    }
+}
